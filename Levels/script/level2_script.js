ca2a27b63ad26a3c9241a3da6b8a426cadeb6c40
woniw <woniwsec@gmail.com>
2026-02-01 09:14:02 -0700

    level 2 functons complete

---
+++ Levels/script/level2_script.js
@@ -0,0 +1,6 @@
+const continue_button = document.getElementById("continue_button");
+const response_label = document.getElementById("response_label");
+
+continue_button.onclick = () => {
+    response_label.innerHTML = "011100110110001101110010011011110110110001101100001000000110010001101111011101110110111000001010"
+}
