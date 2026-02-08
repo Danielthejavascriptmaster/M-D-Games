0ebb126ac05d08c7d405781a261701be98d8f5cf
woniw <woniwsec@gmail.com>
2026-02-08 09:56:22 -0700

    marketplace info button

---
+++ Levels/level4/market/scripts/marketplace_scripts.js
@@ -0,0 +1,8 @@
+const info_button = document.getElementById("info_button");
+const buy_btn_slot_1 = document.getElementById("buy_btn_slot_1");
+const buy_btn_slot_2 = document.getElementById("buy_btn_slot_2");
+const buy_btn_slot_3 = document.getElementById("buy_btn_slot_3")
+info_button.onclick = () => {
+    window.alert("You Can Buy A Business In The Marketplace To Gain More Income");
+    window.alert("Maintenence is needed sometimes you business might fail");
+}
