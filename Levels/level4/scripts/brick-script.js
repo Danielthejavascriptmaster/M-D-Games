a41495f5f29e863aaca43b7825d96ed8a47ffbc3
woniw <woniwsec@gmail.com>
2026-02-12 11:07:27 -0700

    updates

--- Levels/level4/scripts/brick-script.js
+++ Levels/level4/scripts/brick-script.js
@@ -2,6 +2,7 @@ import { business_income, business_risk } from "../../../../utils/business_incom
 business_risk()
 business_income()
 const check_inventory_btn = document.getElementById("check_inventory_btn");
+const coin_label = document.getElementById("coin_label")
 let inventory_label = document.getElementById("inventory");
 let data = JSON.parse(localStorage.getItem("user"));
 
