34ca4ae048d6ca6eb1de9228594317a0b1ff8694
woniw <woniwsec@gmail.com>
2026-02-13 11:43:45 -0700

    guard logic update

---
+++ Levels/level4/downtown/scripts/thegate.js
@@ -0,0 +1,28 @@
+const submit_btn = document.getElementById("submit");
+const check_inventory_btn = document.getElementById("check_inventory_btn");
+let inventory_label = document.getElementById("inventory_label");
+let data = JSON.parse(localStorage.getItem("user"));
+
+check_inventory_btn.onclick = () => {
+    inventory_label.innerHTML = `${data.inventory}`
+}
+
+submit_btn.onclick = () => {
+    const item_1 = document.getElementById("item_1").value;
+
+    if(data.inventory.includes(item_1)){
+        window.alert("user has item")
+        if(item_1 == "pepsi max"){
+            window.alert("Redircting to levle 5")
+        }
+        else if(item_1 == "water"){
+            window.alert("The guard is looking for something more sweet")
+        }
+        else{
+            window.alert("The guard didnt want it")
+        }
+    }
+    else{
+        window.alert("You dont have that item");
+    };
+}
