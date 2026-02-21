c196b9753e325455605637d6fdebfe6ddb0fbff2
woniw <wonisec@gmail.com>
2026-02-21 06:09:42 -0700

    level 5 stat container+stamina

--- Levels_S2/Level5/scripts/level_5_script.js
+++ Levels_S2/Level5/scripts/level_5_script.js
@@ -1,7 +1,20 @@
 import { business_income, business_risk } from "../../../../utils/business_income.js";
-business_income()
-business_risk()
+import { stamina_checker} from "../../../../utils/stamina_script.js"
+let coin_stat = document.getElementById("coin-stat-label");
+let inventory_stat = document.getElementById("inventory-stat-label");
+let stamina_stat = document.getElementById("stamina-stat-label");
 
 let data = JSON.parse(localStorage.getItem("user"));
+coin_stat.innerHTML = `Coins: ${data.coins}`
+inventory_stat.innerHTML = `Inventory: ${data.inventory}`
+stamina_stat.innerHTML = `Stamina: ${data.stamina}`
+
+business_income()
+business_risk()
+stamina_checker()
+
+
+
+
 data.save_levels.level_5 == true
 localStorage.setItem("user", JSON.stringify(data))
